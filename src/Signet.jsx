e925c47ad6603fb0060e4535652d52831378f060
LudovickL <Luberisselud@gmail.com>
2021-02-15 08:16:48 -0500

    ajout d'un bouton pour modifier les signets

--- src/Signet.jsx
+++ src/Signet.jsx
@@ -3,6 +3,7 @@ import './Signet.scss';
 export default function Signet(props) {
     return(
         <div className="Signet">
+            <button>modifier</button>
             <img src={'images-signets/' + props.id + '.png'} alt=""/>
             <div className="info">
                 <p className="titre">{props.titre}</p>
